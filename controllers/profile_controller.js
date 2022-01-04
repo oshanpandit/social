c4afc8c0efaf6b7b66d058965bc58d193d34e4c7
oshan pandit <oshanpandit@gmail.com>
2022-01-04 19:40:58 +0530

    test1

---
+++ controllers/profile_controller.js
@@ -0,0 +1,5 @@
+module.exports.profile=function(req,res){
+
+    res.end('<h1>This is the profile section</h1>')
+
+}
--- index.js
+++ index.js
@@ -6,9 +6,12 @@ const port=8000;
 
 app.use('/',require('./routes/index'));
 
+app.use('/profile',require('./routes/profile'));
+
 app.set('view engine','ejs');
 
 app.set('views','./views');
+
 
 
 app.listen(port,function(err){
