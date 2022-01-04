225f20fa2a2bd1d11086675659d2e4371fb3a6ff
oshan pandit <oshanpandit@gmail.com>
2022-01-04 19:11:52 +0530

    created one more controller and downloaded and used ejs

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,4 +1,5 @@
 module.exports.home=function(req,res){
 
-    return res.end('<h1>The express controller is running!</h1>')
+    return res.end('<h1>The express controller is running!</h1>');
+
 }
--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,6 @@ const homeController=require('../controllers/home_controller');
 
 router.get('/',homeController.home);
 
+router.use('/user',require('./user'));
+
 module.exports=router;
