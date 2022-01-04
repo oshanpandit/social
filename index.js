1db4dc02738971ffec6f3cd13b13117e39c5004e
oshan pandit <oshanpandit@gmail.com>
2022-01-04 13:04:43 +0530

    created the server using express and fired it up

---
+++ index.js
@@ -0,0 +1,19 @@
+const express=require('express');
+
+const app=express();
+
+const port=8000;
+
+
+app.listen(port,function(err){
+
+    if(err){
+
+        console.log(`There was an error while running the server ${err}`);
+
+        return;
+    }
+
+    console.log(`The server is up and running on port : ${port}`);
+    
+});
