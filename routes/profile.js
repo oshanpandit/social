0f4bd7a38f98b71f9ba24b930e5ecd67074b7340
oshan pandit <oshanpandit@gmail.com>
2022-01-16 13:26:36 +0530

    authnticate more

---
+++ routes/profile.js
@@ -0,0 +1,9 @@
+const express=require('express');
+
+const router=express.Router();
+
+const profileController=require('../controllers/profile_controller');
+
+router.get('/',profileController.profile);
+
+module.exports=router;
